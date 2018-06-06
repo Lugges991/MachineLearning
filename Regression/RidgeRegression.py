6a78937adfa90edb0884541d70ae98c237837253
lugges <lugges@localhost.localdomain>
2018-06-06 10:28:26 +0200

    refactoring

---
+++ Regression/RidgeRegression.py
@@ -0,0 +1,45 @@
+################################################################################
+#implementing batch gradient descent using numpy
+#Author: Lucas Mahler
+#GitHub: @Lugges991
+################################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+
+#generate random, linear looking data
+X = 3* np.random.rand(100,1)
+#adding gaussian noise to the graph
+# y = 6 + 2x + gaussian noise
+y = 6 +2 * X + np.random.randn(100,1)
+
+X_new = np.linspace(0,3,100).reshape(100,1)
+def plot_model(model_class, polynomial, alphas, **model_kargs):
+    for alpha, style in zip(alphas, ("b-", "g--", "r:")):
+        model = model_class(alpha, **model_kargs) if alpha > 0 else LinearRegression()
+        if polynomial:
+            model = Pipeline([
+                    ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+                    ("std_scaler", StandardScaler()),
+                    ("regul_reg", model),
+                ])
+        model.fit(X, y)
+        y_new_regul = model.predict(X_new)
+        lw = 2 if alpha > 0 else 1
+        plt.plot(X_new, y_new_regul, style, linewidth=lw, label=r"$\alpha = {}$".format(alpha))
+    plt.plot(X, y, "b.", linewidth=3)
+    plt.legend(loc="upper left", fontsize=15)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 3, 0, 4])
+
+plt.figure(figsize=(8,4))
+plt.subplot(121)
+plot_model(Ridge, polynomial=False, alphas=(0, 10, 100), random_state=42)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(122)
+plot_model(Ridge, polynomial=True, alphas=(0, 10**-5, 1), random_state=42)
+
+save_fig("ridge_regression_plot")
+plt.show()
